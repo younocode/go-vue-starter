88b80b5135dd26005fbf18941df8188071ae0990
tianyang <tianyangchen1@outlook.com>
2025-04-08 22:59:35 +0800

    [feature] initial commit

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 - [VitePress](https://vitepress.dev)
 
 后端
-- 基于 [go-blueprint](https://github.com/Melkeydev/go-blueprint) 初始化
+- 基于 [go-blueprint](https://github.com/Melkeydev/go-blueprint) 初始化，文件目录参考 [project-layout](https://github.com/golang-standards/project-layout)
 - web 框架使用 [Echo](https://echo.labstack.com/)
 
 数据库
--- web/apps/app/vite.config.ts
+++ web/apps/app/vite.config.ts
@@ -1,12 +1,22 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, ''),
+      },
+    },
+  },
   plugins: [
     vue(),
     vueJsx(),
@@ -14,7 +24,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
