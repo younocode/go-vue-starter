99ed80e6a375ebff9d6745ff466930de755cf327
tianyang <tianyangchen1@outlook.com>
2025-04-09 10:14:23 +0800

    [feature] initial commit; add tailwindcss, reka-ui,@iconify/vue

--- web/apps/app/src/App.vue
+++ web/apps/app/src/App.vue
@@ -1,85 +1,82 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { Icon } from '@iconify/vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <PopoverRoot>
+    <PopoverTrigger
+      class="rounded-full w-[35px] h-[35px] inline-flex items-center justify-center text-grass11 bg-white shadow-sm border hover:bg-stone-50 cursor-default focus:shadow-[0_0_0_2px]  focus:shadow-black dark:focus:shadow-green8 focus:outline-none "
+      aria-label="Update dimensions"
+    >
+      <Icon icon="radix-icons:mixer-horizontal" />
+    </PopoverTrigger>
+    <PopoverPortal>
+      <PopoverContent
+        side="bottom"
+        :side-offset="5"
+        class="rounded-lg p-5 w-[260px] bg-white shadow-sm border will-change-[transform,opacity] data-[state=open]:data-[side=top]:animate-slideDownAndFade data-[state=open]:data-[side=right]:animate-slideLeftAndFade data-[state=open]:data-[side=bottom]:animate-slideUpAndFade data-[state=open]:data-[side=left]:animate-slideRightAndFade"
+      >
+        <div class="flex flex-col gap-2.5">
+          <p class="text-mauve12 text-sm leading-[19px] font-semibold mb-2.5">
+            Dimensions
+          </p>
+          <fieldset class="flex gap-5 items-center">
+            <label
+              class="text-xs text-grass11 w-[75px]"
+              for="width"
+            > Width </label>
+            <input
+              id="width"
+              class="w-full inline-flex bg-stone-50 items-center justify-center flex-1 rounded px-2.5 text-xs leading-none text-grass11 shadow-[0_0_0_1px] shadow-green7 h-[25px] focus:shadow-[0_0_0_2px] focus:shadow-green8 outline-none"
+              defaultValue="100%"
+            >
+          </fieldset>
+          <fieldset class="flex gap-5 items-center">
+            <label
+              class="text-xs text-grass11 w-[75px]"
+              for="maxWidth"
+            > Max. width </label>
+            <input
+              id="maxWidth"
+              class="w-full inline-flex bg-stone-50 items-center justify-center flex-1 rounded px-2.5 text-xs leading-none text-grass11 shadow-[0_0_0_1px] shadow-green7 h-[25px] focus:shadow-[0_0_0_2px] focus:shadow-green8 outline-none"
+              defaultValue="300px"
+            >
+          </fieldset>
+          <fieldset class="flex gap-5 items-center">
+            <label
+              class="text-xs text-grass11 w-[75px]"
+              for="height"
+            > Height </label>
+            <input
+              id="height"
+              class="w-full inline-flex bg-stone-50 items-center justify-center flex-1 rounded px-2.5 text-xs leading-none text-grass11 shadow-[0_0_0_1px] shadow-green7 h-[25px] focus:shadow-[0_0_0_2px] focus:shadow-green8 outline-none"
+              defaultValue="25px"
+            >
+          </fieldset>
+          <fieldset class="flex gap-5 items-center">
+            <label
+              class="text-xs text-grass11 w-[75px]"
+              for="maxHeight"
+            > Max. height </label>
+            <input
+              id="maxHeight"
+              class="w-full inline-flex bg-stone-50 items-center justify-center flex-1 rounded px-2.5 text-xs leading-none text-grass11 shadow-[0_0_0_1px] shadow-green7 h-[25px] focus:shadow-[0_0_0_2px] focus:shadow-green8 outline-none"
+              defaultValue="none"
+            >
+          </fieldset>
+        </div>
+        <PopoverClose
+          class="rounded-full h-[25px] w-[25px] inline-flex items-center justify-center text-grass11 absolute top-[8px] right-[8px] hover:bg-green4 focus:shadow-[0_0_0_2px] focus:shadow-green7 outline-none cursor-default"
+          aria-label="Close"
+        >
+          <Icon icon="radix-icons:cross-2" />
+        </PopoverClose>
+        <PopoverArrow class="fill-white stroke-gray-200" />
+      </PopoverContent>
+    </PopoverPortal>
+  </PopoverRoot>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<style lang="scss" scoped>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
--- web/apps/app/vite.config.ts
+++ web/apps/app/vite.config.ts
@@ -1,7 +1,11 @@
 import { fileURLToPath, URL } from 'node:url'
 
+import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import RekaResolver from 'reka-ui/resolver'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
@@ -21,6 +25,34 @@ export default defineConfig({
     vue(),
     vueJsx(),
     vueDevTools(),
+
+    tailwindcss(),
+    //  https://github.com/unplugin/unplugin-vue-components
+    Components({
+      dts: 'src/auto-components.d.ts',
+      resolvers: [
+        RekaResolver(),
+      ],
+    }),
+    // https://github.com/unplugin/unplugin-auto-import
+    AutoImport({
+      dts: 'src/auto-imports.d.ts',
+      // targets to transform
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.vue\.[tj]sx?\?vue/, // .vue (vue-loader with experimentalInlineMatchResource enabled)
+        /\.md$/, // .md
+      ],
+      // global imports to register
+      imports: [
+        // presets
+        'vue',
+        'vue-router',
+        '@vueuse/core',
+      ],
+    }),
   ],
   resolve: {
     alias: {
