462341d716ce9b144fd917a02dc8d8fe2968ffa3
tianyang <tianyangchen1@outlook.com>
2025-04-17 23:45:02 +0800

    [feature] init sql

---
+++ server/internal/database/migration/000001_init_schema.up.sql
@@ -0,0 +1,10 @@
+CREATE EXTENSION IF NOT EXISTS citext;
+
+CREATE TABLE IF NOT EXISTS users
+(
+    id         serial PRIMARY KEY,
+    email      CITEXT UNIQUE NOT NULL, -- 忽略大小写
+    password   VARCHAR(255)  NOT NULL, --  hashed password
+    created_at TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
